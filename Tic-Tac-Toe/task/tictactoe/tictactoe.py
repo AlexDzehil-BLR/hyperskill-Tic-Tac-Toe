e1bbb241c9e618686187ad27e455546a2bb4adad
AlexDzehil-BLR <70115369+AlexDzehil-BLR@users.noreply.github.com>
2020-10-26 11:49:07 +0300

    Stage 5/5: Fight! Good code

--- Tic-Tac-Toe/task/tictactoe/tictactoe.py
+++ Tic-Tac-Toe/task/tictactoe/tictactoe.py
@@ -1,176 +1,34 @@
-
-a = [[' ', ' ', ' '],
-     [' ', ' ', ' '],
-     [' ', ' ', ' ']
-     ]
-per = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']
-
-def valid():
-    per = 'X'
-
-    if per == 'X':
-        print('---------')
-        print('|', a[0][0], a[0][1], a[0][2], '|')
-        print('|', a[1][0], a[1][1], a[1][2], '|')
-        print('|', a[2][0], a[2][1], a[2][2], '|')
-        print('---------')
-        while True:
-            b = input('Enter the coordinates ').split()
-
-            if len(b) != 2:
-                print("You should enter numbers!")
-            else:
-                x, y = b[0], b[1]
-                if str(x).isdigit() == False or str(y).isdigit() == False:
-                    print("You should enter numbers!")
-                elif int(x) <= 0 or int(x) > 3 or int(y) <= 0 or int(y) > 3:
-                    print("Coordinates should be from 1 to 3!")
-                # if X = 1
-                elif x == '1':
-                    if y == '1':
-                        if a[2][0] != ' ':
-                            print("This cell is occupied! Choose another one!")
-                        else:
-                            a[2][0] = per
-
-                    if y == '2':
-                        if a[1][0] != ' ':
-                            print("This cell is occupied! Choose another one!")
-                        else:
-                            a[1][0] = per
-
-                    if y == '3':
-                        if a[0][0] != ' ':
-                            print("This cell is occupied! Choose another one!")
-                        else:
-                            a[0][0] = per
-
-                elif x == '2':
-                    if y == '1':
-                        if a[2][1] != ' ':
-                            print("This cell is occupied! Choose another one!")
-                        else:
-                            a[2][1] = per
-
-                    if y == '2':
-                        if a[1][1] != ' ':
-                            print("This cell is occupied! Choose another one!")
-                        else:
-                            a[1][1] = per
-
-                    if y == '3':
-                        if a[0][1] != ' ':
-                            print("This cell is occupied! Choose another one!")
-                        else:
-                            a[0][1] = per
-
-                elif x == '3':
-                    if y == '1':
-                        if a[2][2] != ' ':
-                            print("This cell is occupied! Choose another one!")
-                        else:
-                            a[2][2] = per
-
-                    if y == '2':
-                        if a[1][2] != ' ':
-                            print("This cell is occupied! Choose another one!")
-                        else:
-                            a[1][2] = per
-
-                    if y == '3':
-                        if a[0][2] != ' ':
-                            print("This cell is occupied! Choose another one!")
-                        else:
-                            a[0][2] = per
+a = list(" " * 9)
+step = 0
 
 
-
-            print('---------')
-            print('|', a[0][0], a[0][1], a[0][2], '|')
-            print('|', a[1][0], a[1][1], a[1][2], '|')
-            print('|', a[2][0], a[2][1], a[2][2], '|')
-            print('---------')
-
-
-def main():
-    valid()
+def printgame():
+    print('-' * 9)
+    for i in range(3):
+        print("| {} {} {} |".format(a[0 + 3 * i], a[1 + 3 * i], a[2 + 3 * i]))
+    print('-' * 9)
 
 
-main()
-
-
-# a = 0
-# b = 0
-#
-# if text[0] == text[1] == text[2] and text[0] != '_':
-#     if a == 0:
-#         a = text[0]
-#     else:
-#         b = text[3]
-# if text[3] == text[4] == text[5] and text[3] != '_':
-#     if a == 0:
-#         a = text[3]
-#     else:
-#         b = text[3]
-# if text[6] == text[7] == text[8] and text[6] != '_':
-#     if a == 0:
-#         a = text[6]
-#     else:
-#         b = text[6]
-# if text[0] == text[3] == text[6] and text[0] != '_':
-#     if a == 0:
-#         a = text[0]
-#     else:
-#         b = text[6]
-# if text[1] == text[4] == text[7] and text[1] != '_':
-#     if a == 0:
-#         a = text[1]
-#     else:
-#         b = text[1]
-# if text[2] == text[5] == text[8] and text[2] != '_':
-#     if a == 0:
-#         a = text[2]
-#     else:
-#         b = text[2]
-# if text[0] == text[4] == text[8] and text[0] != '_':
-#     if a == 0:
-#         a = text[0]
-#     else:
-#         b = text[0]
-# if text[2] == text[4] == text[6] and text[2] != '_':
-#     if a == 0:
-#         a = text[2]
-#     else:
-#         b = text[2]
-#
-# x_count = text.count('X')
-# o_count = text.count('O')
-# x_o = x_count - o_count
-#
-# if a != 0 and b == 0:
-#     print(f"{a} wins")
-# elif a != 0 and b != 0 or x_o > 1 or x_o < -1:
-#     print('Impossible')
-# elif a == 0 and b == 0 and '_' not in text:
-#     print('Draw')
-# elif '_' in text:
-#     print("Game not finished")
-
-# elements = input('Enter cells: ')
-# straights =[elements[:3], elements[3:6], elements[6:], elements[0:9:3], elements[1:9:3], elements[2:9:3], elements[0:9:4],
-# elements[2:7:2]]
-# print('---------')
-# print('|', ' '.join(elements[:3]), '|')
-# print('|',' '.join(elements[3:6]),'|')
-# print('|',' '.join(elements[6:]),'|')
-# print('---------')
-# if abs(elements.count('X') - elements.count('O')) > 1 or ('XXX' in straights and 'OOO' in straights):
-#     print('Impossible')
-# elif 'XXX' in straights:
-#     print('X wins')
-# elif 'OOO' in straights:
-#     print('O wins')
-# elif elements.count('_') > 0:
-#     print('Game not finished')
-# elif elements.count('_') == 0:
-#     print('Draw')
+while " " in a:
+    playx, playy = input("Enter the coordinates:").split()
+    if (int(playx) or int(playy)) not in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]:
+        print("You should enter numbers!")
+    elif (int(playx) or int(playy)) not in [1, 2, 3]:
+        print("Coordinates should be from 1 to 3!")
+    elif a[(int(playx) - 1) + abs(int(playy) - 3) * 3] != " ":
+        print("This cell is occupied! Choose another one!")
+    else:
+        if step == 0:
+            a[(int(playx) - 1) + abs(int(playy) - 3) * 3] = "X"
+            step = 1
+        elif step == 1:
+            a[(int(playx) - 1) + abs(int(playy) - 3) * 3] = "O"
+            step = 0
+        printgame()
+        rule = [a[:3], a[3:6], a[6:], a[0:9:3], a[1:9:3], a[2:9:3], a[0:9:4], a[2:7:2]]
+        if ['X', 'X', 'X'] in rule:
+            print("X wins")
+        elif ['O', 'O', 'O'] in rule:
+            print("O wins")
+        elif a.count(" ") == 0:
+            print("Draw")
